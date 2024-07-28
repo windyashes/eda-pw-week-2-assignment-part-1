bf50a9824c5cecbebf6b8469c72a67db6b42fce0
windyashes <sethkemmet@gmail.com>
2024-07-28 09:41:23 -0500

    addedallowedPets var + added result var with conditional.

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -35,12 +35,18 @@ let friendsPets = 2;
 // 10 - Add two pets to your `pets` variable
 pets += 2;
 // 11 - Create a constant variable called `allowedPets` and set it to a number value of your choice
-
+const allowedPets = 2;
 // 12 - Create a variable called `result`. Create a conditional:
 //      if adventurous is true, set `result` to be "Adventures are great!", 
 //      if it's not true,  set `result` to be "How about we stay home?"
 //      Console log the value of `result`
-
+let result;
+if(adventurous) {
+    result = 'Adventures are great!'
+  } else {
+    result = 'How about we stay home?'
+  };
+  console.log(result);
 // 13 - Create a variable called `diceRoll` and set it to the value of "Try again later.".
 //      Create a compound conditional: 
 //      if luckyNumber is 2 and adventurous is true,
